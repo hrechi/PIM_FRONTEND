e931a3b1671e44fa3f20167bae13e01c900fd840
Mohamed Yessine Nahdi <MohamedYessine.Nahdi@esprit.tn>
2026-04-05 13:00:37 +0100

    refactor: update plant image handling to use memory bytes and synchronize project dependencies

--- lib/screens/plant_doctor_screen.dart
+++ lib/screens/plant_doctor_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
@@ -15,7 +16,8 @@ class PlantDoctorScreen extends StatefulWidget {
 }
 
 class _PlantDoctorScreenState extends State<PlantDoctorScreen> {
-  File? _selectedImage;
+  XFile? _selectedImage;
+  Uint8List? _imageBytes;
   bool _isLoading = false;
   Map<String, dynamic>? _aiResult;
 
@@ -30,15 +32,17 @@ class _PlantDoctorScreenState extends State<PlantDoctorScreen> {
   Future<void> _pickImage(ImageSource source) async {
     final XFile? image = await _picker.pickImage(source: source);
     if (image != null) {
+      final bytes = await image.readAsBytes();
       setState(() {
-        _selectedImage = File(image.path);
+        _selectedImage = image;
+        _imageBytes = bytes;
         _aiResult = null; // Reset previous results
       });
     }
   }
 
   Future<void> _analyzePlant() async {
-    if (_selectedImage == null) return;
+    if (_selectedImage == null || _imageBytes == null) return;
 
     setState(() {
       _isLoading = true;
@@ -47,7 +51,11 @@ class _PlantDoctorScreenState extends State<PlantDoctorScreen> {
     try {
       // 1. Prepare the request
       var request = http.MultipartRequest('POST', Uri.parse(_apiUrl));
-      request.files.add(await http.MultipartFile.fromPath('file', _selectedImage!.path));
+      request.files.add(http.MultipartFile.fromBytes(
+        'file', 
+        _imageBytes!, 
+        filename: _selectedImage!.name,
+      ));
 
       // 2. Send to Python Backend
       var streamedResponse = await request.send();
@@ -101,8 +109,8 @@ class _PlantDoctorScreenState extends State<PlantDoctorScreen> {
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(20),
                   border: Border.all(color: AppColorPalette.emeraldGreen.withOpacity(0.5), width: 2),
-                  image: _selectedImage != null
-                      ? DecorationImage(image: FileImage(_selectedImage!), fit: BoxFit.cover)
+                  image: (_selectedImage != null && _imageBytes != null)
+                      ? DecorationImage(image: MemoryImage(_imageBytes!), fit: BoxFit.cover)
                       : null,
                 ),
                 child: _selectedImage == null
