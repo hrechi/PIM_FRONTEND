c30553e965931568f9e0a74f56fab35476b11166
amelmediouni2001 <amel.mediouni@esprit.tn>
2026-04-20 13:58:04 +0100

    changing the ip again

--- lib/screens/security/live_feed_screen.dart
+++ lib/screens/security/live_feed_screen.dart
@@ -164,7 +164,7 @@ class _LiveFeedScreenState extends State<LiveFeedScreen> {
               style: TextStyle(color: AppColorPalette.charcoalGreen),
               keyboardType: TextInputType.url,
               decoration: InputDecoration(
-                hintText: 'e.g. 192.168.1.182',
+                hintText: 'e.g. 192.168.1.183',
                 hintStyle: TextStyle(
                   color: AppColorPalette.softSlate.withValues(alpha: 0.5),
                 ),
